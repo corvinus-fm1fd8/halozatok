ff8e202ec961d2e23cb5214aff4751ad87a15c42
Molcsán Anna <molcsananna@gmail.com>
2021-03-19 10:02:09 +0100

    6. hét-javascript gyakorlás

---
+++ HajosTeszt/HajosTeszt/wwwroot/jsgyak.js
@@ -0,0 +1,34 @@
+﻿
+var faktorialis = function(n) {
+    let result = 1;
+    for (let i = 1; i <= n; i++) {
+        result = result * i;
+    }
+    return result;
+}
+
+function pascal(n, k)
+{
+    return faktorialis(n) / (faktorialis(k) * faktorialis(n-k));
+}
+
+
+
+window.onload = () =>
+{
+    for (var s = 0; s < 10; s++) {
+        var sor = document.createElement("div");
+        sor.className = "sor";
+        document.getElementById("pascal").appendChild(sor);
+
+        for (var o = 0; o < s; o++) {
+            var oszlop = document.createElement("div");
+            sor.appendChild(oszlop);
+            oszlop.className = "elem";
+            oszlop.innerHTML = `${pascal(s, o)}`;
+            oszlop.style.backgroundColor = `rgb(${227 - pascal(s, o)},
+${228 - pascal(s, o)},${255 - pascal(s, o)})`;
+        }
+
+    }
+}

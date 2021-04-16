5d9a220c0bff2e93d577816d0f7ca3bef1368921
Molcsán Anna <molcsananna@gmail.com>
2021-04-16 09:47:51 +0200

    számháló- 8.hét

---
+++ HajosTeszt/HajosTeszt/Controllers/TesztController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SzamHal.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TesztController : ControllerBase
+    {
+        [HttpGet]
+
+        //get típusú HTTP kérésre válaszol a fv.
+        [Route("corvinus/szerverido")]
+        //beállítja, hogy milyen route-on legyen elérhető az endpoint
+        public IActionResult M1()
+        {
+            string pontosIdő = DateTime.Now.ToShortTimeString();
+
+            return new ContentResult
+            {
+
+                ContentType = System.Net.Mime.MediaTypeNames.Text.Plain, //"text/plain"
+                Content = pontosIdő //string típusú (visszaküldendő válasz)
+
+            };
+        }
+
+    }
+}
